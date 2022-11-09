aaaafb09d815035489ec1a1a32281a49356761a4
Sirio <sirio.iasevoli@gmail.com>
2022-11-09 17:57:17 +0100

    calcolo più snello

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ for (let i= 1; i<= 100; i++){
     element.classList.add("box");
     container.append(element);
 
-    if ((i % 3 ) === 0 && !((i % 5) === 0 )){
-        element.innerHTML = `fizz`;
-        element.classList.add("fizz");
-    }else if((i % 5) === 0 && !((i % 3) === 0 )){
-        element.innerHTML = `buzz`;
-        element.classList.add("buzz");
-    }else if((i % 5) === 0 && (i % 3) === 0){
+    if ((i % 3 ) === 0 && ((i % 5) === 0 )){
         element.innerHTML = `fizzbuzz`;
         element.classList.add("fizzbuzz");
+    }else if((i % 5) === 0){
+        element.innerHTML = `buzz`;
+        element.classList.add("buzz");
+    }else if((i % 3) === 0){
+        element.innerHTML = `fizz`;
+        element.classList.add("fizz");
     }else{
         element.classList.add("normal");
     }
